be6c704f95ffd02fbcd1a3fe71513bf826ab5a84
tnromero <tnromero@gmail.com>
2013-11-30 02:09:53 -0200

    Detalhes Pedido com ERRO

--- src/com/example/xcsbooks/DetalhesPedidoActivity.java
+++ src/com/example/xcsbooks/DetalhesPedidoActivity.java
@@ -1,9 +1,19 @@
 package com.example.xcsbooks;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.example.xcsbooks.control.GetBookCover;
+import com.example.xcsbooks.control.JSONParser;
 import com.example.xcsbooks.model.Dinheiro;
+import com.example.xcsbooks.model.LivroNovo;
 import com.example.xcsbooks.model.Pedido;
+import com.example.xcsbooks.model.Produto;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,28 +25,21 @@ import android.widget.TextView;
 
 public class DetalhesPedidoActivity extends BaseActivity {
 
-	private TextView mTxtIdPedido;
-	private TextView mTxtDatahoraPedido;
-	private TextView mTxtTotalPedido;
-	private TextView mTxtEstadPedido;
 	private Pedido pedido;
 	private ListView mLv; 
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detalhes_pedido);
-		
-		
-		
-		
-		
+				
 		mLv = (ListView) findViewById(R.id.detalhePedido_listaItensView);
 		View v = getLayoutInflater().inflate(R.layout.detalhe_pedido_header, null);
-		mTxtIdPedido = (TextView) findViewById(R.id.detalhePedido_txtId);
-		mTxtDatahoraPedido = (TextView) findViewById(R.id.detalhePedido_txtDatahora);
-		mTxtEstadPedido = (TextView) findViewById(R.id.detalhePedido_txtEstado);
-		mTxtTotalPedido = (TextView) findViewById(R.id.detalhePedido_txtTotal);
+		TextView mTxtIdPedido = (TextView) findViewById(R.id.detalhePedido_txtId);
+		TextView mTxtDatahoraPedido = (TextView) findViewById(R.id.detalhePedido_txtDatahora);
+		TextView mTxtEstadPedido = (TextView) findViewById(R.id.detalhePedido_txtEstado);
+		TextView mTxtTotalPedido = (TextView) findViewById(R.id.detalhePedido_txtTotal);
 		mLv.addHeaderView(v);
 		
 		//Obtem o objeto pedido passado
@@ -44,14 +47,39 @@ public class DetalhesPedidoActivity extends BaseActivity {
 		Resources r = getResources();
 		
 		pedido = (Pedido) i.getParcelableExtra(AcompanharPedidoActivity.KEY_PEDIDO);
+		if(pedido.getId()!=0){
+			mTxtIdPedido.setText(r.getString(R.string.id) + ": " +pedido.getId());
+			mTxtDatahoraPedido.setText(r.getString(R.string.datahora)+": "+pedido.getDatahora());
+			mTxtEstadPedido.setText(r.getString(R.string.status) + ": " + pedido.getEstado());
+			mTxtTotalPedido.setText(r.getString(R.string.total) + ": " + pedido.getTotal().toString());
+		}
 		
-		mTxtIdPedido.setText(r.getString(R.string.id) + ": " +pedido.getId());
-		mTxtDatahoraPedido.setText(r.getString(R.string.datahora)+": "+pedido.getDatahora());
-		mTxtEstadPedido.setText(r.getString(R.string.status) + ": " + pedido.getEstado());
-		mTxtTotalPedido.setText(r.getString(R.string.total) + ": " + pedido.getTotal().toString());
+		/*
+		List searchList = new ArrayList();
+		Map map = null;
+		for(Produto p : pedido.getProdutos()) {
+			map = new HashMap();
+			map.put("itemLista_thumbLivro", GetBookCover.getCover(((LivroNovo)p).getIsbn()));
+			map.put("itemLista_tituloLivro", ((LivroNovo)p).getTitulo());
+			map.put("itemLista_autorLivro", ((LivroNovo)p).getAutor());
+			searchList.add(map);
+		}
+		String[] t = {"itemLista_thumbLivro","itemLista_tituloLivro",
+				"itemLista_autorLivro"};
+		
+		int[] item = {R.id.itemLista_thumbLivro,
+				R.id.itemLista_tituloLivro, R.id.itemLista_autorLivro};
+	
+		//Adapter
+
+		final ExtendedSimpleAdapter adapter = new ExtendedSimpleAdapter(this, (List<HashMap<String, Object>>) searchList, R.layout.item_lista, t , item);
+		
+		mLv.setAdapter(adapter);
+		*/
 		
 		
 	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
